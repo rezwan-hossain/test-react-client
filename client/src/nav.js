81318c98dd02e6826b47b59f3c2b5a6accaeec21
Rezwan Hossain <rezwanvoot@gmail.com>
2021-03-18 11:17:44 +0600

    some minor tweke

--- client/src/nav.js
+++ client/src/nav.js
@@ -2,22 +2,80 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Nav = () => {
+  const [navbarOpen, setNavbarOpen] = React.useState(false);
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="create">Create</Link>
-        </li>
-        <li>
-          <Link to="signup">Sign up</Link>
-        </li>
-        <li>
-          <Link to="login">Sign in</Link>
-        </li>
-      </ul>
+    // <nav>
+    //   <ul>
+    //     <li>
+    //       <Link to="/">Home</Link>
+    //     </li>
+    //     <li>
+    //       <Link to="create">Create</Link>
+    //     </li>
+    //     <li>
+    //       <Link to="signup">Sign up</Link>
+    //     </li>
+    //     <li>
+    //       <Link to="login">Sign in</Link>
+    //     </li>
+    //   </ul>
+    // </nav>
+
+    <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg bg-indigo-500 mb-3">
+      <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+        <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+          <a
+            className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase text-white"
+            href="#pablo"
+          >
+            <Link to="/">Home</Link>
+          </a>
+          <button
+            className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+            type="button"
+            onClick={() => setNavbarOpen(!navbarOpen)}
+          >
+            <i className="fas fa-bars"></i>
+          </button>
+        </div>
+        <div
+          className={
+            "lg:flex flex-grow items-center" +
+            (navbarOpen ? " flex" : " hidden")
+          }
+          id="example-navbar-danger"
+        >
+          <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+            <li className="nav-item">
+              <a
+                className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                href="#pablo"
+              >
+                <i className="fab fa-facebook-square text-lg leading-lg text-white opacity-75"></i>
+                <Link to="create">Create</Link>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                href="#pablo"
+              >
+                <i className="fab fa-twitter text-lg leading-lg text-white opacity-75"></i>
+                <Link to="signup">Sign up</Link>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                href="#pablo"
+              >
+                <i className="fab fa-pinterest text-lg leading-lg text-white opacity-75"></i>
+                <Link to="login">Sign in</Link>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 };
