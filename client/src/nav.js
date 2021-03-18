e9f5a2b409f5a8d3aef43abab6ba0f46635a0c47
Rezwan Hossain <rezwanvoot@gmail.com>
2021-03-18 10:51:48 +0600

    some styly

--- client/src/nav.js
+++ client/src/nav.js
@@ -1,20 +1,25 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-const Nav = ()=>{
-    return(
+const Nav = () => {
+  return (
     <nav>
-        <ul>
-            <li>
-                <Link to='/'>Home</Link>
-            </li>
-            <li>
-                <Link to="create">Create</Link>
-            </li>
-        </ul>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="create">Create</Link>
+        </li>
+        <li>
+          <Link to="signup">Sign up</Link>
+        </li>
+        <li>
+          <Link to="login">Sign in</Link>
+        </li>
+      </ul>
     </nav>
-    )
-}
-
+  );
+};
 
 export default Nav;
--- client/src/routes.js
+++ client/src/routes.js
@@ -4,6 +4,8 @@ import App from "./App";
 import Create from "./create";
 import SinglePost from "./singlePost";
 import UpdatePost from "./updatePost";
+import SignUp from "./signup";
+import SignIn from "./login";
 
 const Routes = () => {
   return (
@@ -11,6 +13,8 @@ const Routes = () => {
       <Switch>
         <Route path="/" exact component={App} />
         <Route path="/create" exact component={Create} />
+        <Route path="/signup" exact component={SignUp} />
+        <Route path="/login" exact component={SignIn} />
         <Route path="/post/:slug" exact component={SinglePost} />
         <Route path="/update/:slug" exact component={UpdatePost} />
       </Switch>
